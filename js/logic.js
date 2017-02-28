306c9a7523e4817cd028cb1f04bd8cebec55b469
Yves Alain Roulin Peier <y.roulin@gmail.com>
2017-02-28 13:44:38 -0600

    Ajustes en estilos / validacion

--- js/logic.js
+++ js/logic.js
@@ -1,25 +1,23 @@
-	//CAPTURA DE ELEMENTOS.
-
-    
-
-    //EVENT LISTENERS PARA BOTONES DE TRANSFERIR.
-    document.querySelector('#btnTransferirB').addEventListener('click', function(){
-			    var saldoA = Number(document.querySelector('#txtSaldoA').value),
+document.querySelector('#btnTransferirB').addEventListener('click', function(){
+ 				var saldoA = Number(document.querySelector('#txtSaldoA').value),
 					saldoB = Number(document.querySelector('#txtSaldoB').value),
 					montoA = Number(document.querySelector('#txtMontoA').value),
 					montoB = Number(document.querySelector('#txtMontoB').value);
 					    
 			    if(saldoA < montoA || montoA === ''){
+			    	document.querySelector('#campo1').classList.add('empty');
 			    	document.querySelector('#txtMontoA').classList.add('error');
+			    	document.querySelector('#txtMontoA').value = '';
 			    } else {
 			    	document.querySelector('#txtSaldoA').value = (saldoA - montoA);
 					document.querySelector('#txtSaldoB').value = (montoA + saldoB);
 					document.querySelector('#txtMontoA').value = '';
 					document.querySelector('#txtMontoA').classList.remove('error');
-			    }
-    });
+			    }			   
+});
 
-    document.querySelector('#btnTransferirA').addEventListener('click', function(){
+    document.querySelector('#btnTransferirA').addEventListener('click', function()
+    {
     		var saldoA = Number(document.querySelector('#txtSaldoA').value),
 			    saldoB = Number(document.querySelector('#txtSaldoB').value),
 			    montoA = Number(document.querySelector('#txtMontoA').value),
@@ -27,6 +25,7 @@
 
     		if(saldoB < montoB || montoB === '') {
 			    	document.querySelector('#txtMontoB').classList.add('error');
+			    	document.querySelector('#txtMontoB').value = '';
     		} else {
     			document.querySelector('#txtSaldoB').value = (saldoB - montoB);
 			    document.querySelector('#txtSaldoA').value = (montoB + saldoA);
